a90e4710ad70da3fd5b4d6db38604edc8dba3dfb
Kevin Luna <L21020397@veracruz.tecnm.mx>
2026-05-14 20:17:58 -0600

    feat(routing): add lazy routes for buscar and registrar pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'buscar',
+    pathMatch: 'full',
+  },
+  {
+    path: 'buscar',
+    loadComponent: () =>
+      import('./pages/search/search-by-id.component').then(
+        (m) => m.SearchByIdComponent
+      ),
+  },
+  {
+    path: 'registrar',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'buscar',
+  },
+];
