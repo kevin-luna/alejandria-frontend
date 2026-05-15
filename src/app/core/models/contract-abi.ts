0b6c24ab910108b05c78e18a0d16e045fd29567b
Kevin Luna <L21020397@veracruz.tecnm.mx>
2026-05-14 20:20:06 -0600

    feat(models): add smart contract ABI definition

---
+++ src/app/core/models/contract-abi.ts
@@ -0,0 +1,91 @@
+export const ALEJANDRIA_ABI = [
+  {
+    name: 'getPublication',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'id', type: 'uint256' }],
+    outputs: [
+      {
+        name: '',
+        type: 'tuple',
+        components: [
+          { name: 'id', type: 'uint256' },
+          { name: 'title', type: 'string' },
+          { name: 'authorNames', type: 'string[]' },
+          { name: 'authorAddresses', type: 'address[]' },
+          { name: 'pubType', type: 'uint8' },
+          { name: 'registrationDate', type: 'uint256' },
+          { name: 'contentHash', type: 'bytes32' },
+          { name: 'institution', type: 'string' },
+          { name: 'doi', type: 'string' },
+          { name: 'ipfsHash', type: 'string' },
+          { name: 'registrant', type: 'address' },
+          { name: 'isActive', type: 'bool' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'totalPublications',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [{ name: '', type: 'uint256' }],
+  },
+  {
+    name: 'verifyByHash',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'contentHash', type: 'bytes32' }],
+    outputs: [
+      { name: 'registered', type: 'bool' },
+      { name: 'id', type: 'uint256' },
+    ],
+  },
+  {
+    name: 'getByRegistrant',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'registrant', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256[]' }],
+  },
+  {
+    name: 'getByAuthor',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: 'author', type: 'address' }],
+    outputs: [{ name: '', type: 'uint256[]' }],
+  },
+  {
+    name: 'register',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      {
+        name: 'p',
+        type: 'tuple',
+        components: [
+          { name: 'title', type: 'string' },
+          { name: 'authorNames', type: 'string[]' },
+          { name: 'authorAddresses', type: 'address[]' },
+          { name: 'pubType', type: 'uint8' },
+          { name: 'contentHash', type: 'bytes32' },
+          { name: 'institution', type: 'string' },
+          { name: 'doi', type: 'string' },
+          { name: 'ipfsHash', type: 'string' },
+        ],
+      },
+    ],
+    outputs: [{ name: 'id', type: 'uint256' }],
+  },
+  {
+    name: 'PublicationRegistered',
+    type: 'event',
+    inputs: [
+      { name: 'id', type: 'uint256', indexed: true },
+      { name: 'registrant', type: 'address', indexed: true },
+      { name: 'contentHash', type: 'bytes32', indexed: true },
+      { name: 'pubType', type: 'uint8', indexed: false },
+    ],
+  },
+] as const;
